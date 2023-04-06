0d2ba8e141552598a3b0d634f5d589069f09d2d5
kathuluuu <kthlnnvrr@gmail.com>
2023-04-06 13:08:52 -0700

    update config

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
   $db_host = 'us-west.connect.psdb.cloud';
   $db_name = "nursing-vr-dev";
-  $db_username = 'n6zfvzbog0zcpp1y3t5k';
-  $db_password = 'pscale_pw_eebZyF8tOJPkG5wjfi15GJq9w4ahI2Vae1IoU1dZEMd';
+  $db_username = 'lbofwb9ke4cnjnld9vw8';
+  $db_password = 'pscale_pw_DpzvcwwElY1otbOBOYAHjSsLCDumvJdfnfmFtkHjTv4';
 
 ?>
