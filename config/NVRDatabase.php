28bf2bbb27a9deaf6cfdd8c73ad85567f0935b6c
kathuluuu <kthlnnvrr@gmail.com>
2023-04-18 19:35:57 -0700

    hng

--- config/NVRDatabase.php
+++ config/NVRDatabase.php
@@ -1,11 +1,10 @@
 <?php
 class Database
 {
-    include_once("config.php");
-    // private $host = "us-west.connect.psdb.cloud";
-    // private $db_name = 'NursingVR';
-    // private $username = 'n6zfvzbog0zcpp1y3t5k';
-    // private $password = 'pscale_pw_eebZyF8tOJPkG5wjfi15GJq9w4ahI2Vae1IoU1dZEMd';
+    private $host = "us-west.connect.psdb.cloud";
+    private $db_name = 'NursingVR';
+    private $username = 'n6zfvzbog0zcpp1y3t5k';
+    private $password = 'pscale_pw_eebZyF8tOJPkG5wjfi15GJq9w4ahI2Vae1IoU1dZEMd';
     private $conn;
 
     public function connect()
--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
   $db_host = 'us-west.connect.psdb.cloud';
   $db_name = "nursing-vr-dev";
-  $db_username = 'lbofwb9ke4cnjnld9vw8';
-  $db_password = 'pscale_pw_DpzvcwwElY1otbOBOYAHjSsLCDumvJdfnfmFtkHjTv4';
+  $db_username = 'm6vn83cyxbctrk6iff67';
+  $db_password = 'pscale_pw_po3HuSIhbVD8qexMtxTpoW3hbHAP1Emaqpnv9IPpDTL';
 
 ?>
