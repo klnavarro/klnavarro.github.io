56b5fa4f516da2d2650d29464a66614a0e45ec85
Kathleen Navarro <kthlnnvrr@gmail.com>
2024-08-26 12:58:47 -0700

    portfolio template

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  $db_host = 'us-west.connect.psdb.cloud';
-  $db_name = "nursing-vr-dev";
-  $db_username = 'm6vn83cyxbctrk6iff67';
-  $db_password = 'pscale_pw_po3HuSIhbVD8qexMtxTpoW3hbHAP1Emaqpnv9IPpDTL';
-
-?>
