86bc3e7e6587fe9ea788f9bf1fbc61651dd5eaaa
Kathleen Navarro <kthlnnvrr@gmail.com>
2025-04-25 13:46:13 -0700

    services

--- kn-portfolio/src/Components/About/About.jsx
+++ kn-portfolio/src/Components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './About.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/about_profile.svg'
 
 const About = () => {
   return (
@@ -12,12 +13,53 @@ const About = () => {
 
       <div className="about-sections">
         <div className="about-left">
-
+          <img src={profile_img} alt="" />
         </div>
+
         <div className="about-right">
-          
+          <div className="about-para">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, neque.</p>
+          <p>Dolorum, quia dignissimos asperiores doloribus libero dolore magnam nesciunt totam!</p>
+          </div>
+
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{width: "50%"}}/>
+            </div>
+            <div className="about-skill">
+              <p>Ruby on Rails</p>
+              <hr style={{width: "50%"}}/>
+            </div>
+            <div className="about-skill">
+              <p>LAMP</p>
+              <hr style={{width: "50%"}}/>
+            </div>
+            <div className="about-skill">
+              <p>React</p>
+              <hr style={{width: "50%"}}/>
+            </div>
+          </div>
         </div>
       </div>
+      <div className="about-achievments">
+        <div className="about-achievement">
+          <h1>7</h1>
+          <p>Years of Experience</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>7+</h1>
+          <p>Years of Experience</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>Happy Clients</p>
+        </div>
+      </div>
+
+
     </div>
   )
 }
---
+++ kn-portfolio/src/Components/Services/Services.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Services.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import services_data from '../../assets/services_data.js'
+
+const Services = () => {
+  return (
+    <div className='services'>
+      <div className='services-title'>
+        <h1>My Services</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="services-container">
+        {services_data.map((service,index)=>{
+          return <div className='services-format'>
+            <h3>{service.s_no}</h3>
+            <h2>{service.s_name}</h2>
+          </div>
+        })}
+      </div>
+      
+    </div>
+  )
+}
+
+export default Services
