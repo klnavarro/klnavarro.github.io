b26da0be83e7103240fa762203a467aed7ea8804
Kathleen Navarro <kthlnnvrr@gmail.com>
2025-05-04 17:00:43 -0700

    services

--- kn-portfolio/src/Components/Services/Services.jsx
+++ kn-portfolio/src/Components/Services/Services.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Services.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import services_data from '../../assets/services_data.js'
+import arrow_icon from '../../assets/arrow_icon.svg'
 
 const Services = () => {
   return (
@@ -15,6 +16,11 @@ const Services = () => {
           return <div className='services-format'>
             <h3>{service.s_no}</h3>
             <h2>{service.s_name}</h2>
+            <p>{service.s_desc}</p>
+            <div className='services-readmore'>
+              <p>Read More</p>
+              <img src={arrow_icon} alt="" />
+            </div>
           </div>
         })}
       </div>
